2473fe58a1ba75125f06021aba60700455307646
OnlyJaximus <56784702+OnlyJaximus@users.noreply.github.com>
2022-11-10 04:42:11 -0800

    Update navbar-top.php

--- admin/includes/navbar-top.php
+++ admin/includes/navbar-top.php
@@ -1,6 +1,6 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-primary">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
+     <a class="navbar-brand ps-3" href="../index.php">Look at the User Page</a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     <!-- Navbar Search-->
